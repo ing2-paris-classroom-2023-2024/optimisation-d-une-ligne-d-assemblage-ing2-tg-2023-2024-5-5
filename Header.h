265312f6d1f8ced7ebfeb724f30a737dab1f28e9
Octaveeee <osaveaux@gmail.com>
2023-12-02 00:47:33 +0100

    modif header

--- Header.h
+++ Header.h
@@ -1,9 +1,20 @@
 //
 // Created by osave on 21/11/2023.
 //
+#include <stdbool.h>
 
 #ifndef PROJET_ING2_HEADER_H
 #define PROJET_ING2_HEADER_H
+
+struct Arc
+{
+    int sommet; // numéro de sommet d'un arc adjacent au sommet initial
+    int valeur;
+    struct Arc* arc_suivant;
+};
+
+/* Alias de pointeur sur un Arc */
+typedef struct Arc* pArc;
 
 
 
@@ -13,16 +24,23 @@ typedef struct tache
     int numero;
     float temps;
     int degre;
+
     int *exclusion;
+
     int *precedences;
     int nb_de_prece;
 
+    bool marquage;
+
+    struct Arc* arc;
 }taches;
 
 
 typedef struct station
 {
-    int *station;
+    int ordre;
+    int *tab_station;
+    struct station* suivant;
 }t_station;
 
 typedef struct graphe
