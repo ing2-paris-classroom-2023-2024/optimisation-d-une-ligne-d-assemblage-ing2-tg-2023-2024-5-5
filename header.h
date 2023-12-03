47f362dd0f2e002cc4f721761fb6d4246a6125d0
Octaveeee <osaveaux@gmail.com>
2023-12-03 12:34:01 +0100

    commit test

---
+++ header.h
@@ -0,0 +1,42 @@
+//
+// Created by osave on 03/12/2023.
+//
+#include <stdbool.h>
+
+#ifndef PROJET_ING2_TEST_HEADER_H
+#define PROJET_ING2_TEST_HEADER_H
+
+
+typedef struct station
+{
+    int ordre;
+    float temps_tot;
+    int *tab_station;
+    struct station* suivant;
+}t_station;
+
+typedef struct tache
+{
+    int numero;
+    float temps;
+    int degre;
+
+    int *exclusion;
+
+    int *precedences;
+    int nb_de_prece;
+
+    bool marquage;
+
+}taches;
+
+typedef struct graphe
+{
+    int ordre;
+    float temps_de_cycle;
+    taches *tache;
+    t_station *ancre;
+}t_graphes;
+
+
+#endif //PROJET_ING2_TEST_HEADER_H
